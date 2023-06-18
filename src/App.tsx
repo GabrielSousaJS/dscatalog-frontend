e8afc3822b9fb530ae8315590f89d9ab2d49b0d9
GabrielSousaJS <gabriels.santosx@gmail.coom>
2023-06-18 09:22:52 -0300

    Estilização da Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import Navbar from './components/Navbar';
+import './assets/styles/custom.scss';
 import './App.css';
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Navbar />
+      <h1>Hello Word</h1>
+    </>
   );
 }
 
