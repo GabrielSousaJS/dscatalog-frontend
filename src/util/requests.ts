346e21b0116f5d289ee3ba44d0e6997e517da21a
GabrielSousaJS <gabriels.santosx@gmail.coom>
2023-06-21 18:48:20 -0300

    Salvando os dados de autorização no localStorage

--- src/util/requests.ts
+++ src/util/requests.ts
@@ -1,33 +1,53 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import qs from 'qs';
 
+type LoginResponse = {
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+  scope: string;
+  userFirstName: string;
+  userId: number;
+};
+
 export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+
+const tokenKey = 'authData';
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? 'dscatalog';
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_ID ?? 'dscatalog123';
 
 type LoginData = {
-    username: string;
-    password: string;
-}
+  username: string;
+  password: string;
+};
 
 export const requestBackendLogin = (loginData: LoginData) => {
-    const data = qs.stringify( {
-        ...loginData,
-        grant_type: 'password'
-    });
+  const data = qs.stringify({
+    ...loginData,
+    grant_type: 'password',
+  });
 
-    const params: AxiosRequestConfig = {
-        method: 'POST',
-        baseURL: BASE_URL,
-        url: '/oauth/token',
-        data,
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization': 'Basic ' + window.btoa(CLIENT_ID + ":" + CLIENT_SECRET)
-        }
-    }
+  const params: AxiosRequestConfig = {
+    method: 'POST',
+    baseURL: BASE_URL,
+    url: '/oauth/token',
+    data,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: 'Basic ' + window.btoa(CLIENT_ID + ':' + CLIENT_SECRET),
+    },
+  };
 
-    return axios(params);
+  return axios(params);
+};
 
+export const saveAuthData = (obj: LoginResponse) => {
+    localStorage.setItem(tokenKey, JSON.stringify(obj));
+}
+
+export const getAuthData = () => {
+    const str = localStorage.getItem(tokenKey) ?? "{}";
+    return JSON.parse(str) as LoginResponse;
+    
 }
